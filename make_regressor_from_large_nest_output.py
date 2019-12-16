9623cf030638debf14a947b05fa328b57d6645d0
Mikkel Elle Lepperød <lep.mik@gmail.com>
2019-12-16 17:45:40 +0100

    sys arg in making regressors

--- make_regressor_from_large_nest_output.py
+++ make_regressor_from_large_nest_output.py
@@ -3,8 +3,10 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import json
+import sys
 
-path = pathlib.Path('/media/storage/causal_opto_data/results_400k/')
+
+path = pathlib.Path(sys.argv[1])
 
 with open(str(path / 'params.json'), 'r') as f:
     params = json.load(f)
@@ -19,13 +21,14 @@ stim_times = np.array(stim_data['times'])
 
 N = len(sender_ids)
 
-def make_regressors(stim_times, spikes, z1=-2, z2=0, x1=1, x2=3, y1=3, y2=7):
+def make_regressors(stim_times, spikes, z1=-2, z2=0, x1=1, x2=3, y1=3, y2=7, yb1=-4, yb2=0):
     senders = spikes.sender
     times = spikes.times
     min_sender_ids = min(sender_ids)
     z = np.zeros((len(stim_times), N))
     x = np.zeros((len(stim_times), N))
     y = np.zeros((len(stim_times), N))
+    yb = np.zeros((len(stim_times), N))
     for i, t in enumerate(tqdm(stim_times)):
         idx = np.searchsorted(times, [t + z1, t + z2], side='right')
         id_spiked_before = senders[idx[0]: idx[1]] - min_sender_ids
@@ -39,11 +42,18 @@ def make_regressors(stim_times, spikes, z1=-2, z2=0, x1=1, x2=3, y1=3, y2=7):
         idx = np.searchsorted(times, [t + y1, t + y2], side='right')
         id_spiked = senders[idx[0]: idx[1]] - min_sender_ids
         y[i, id_spiked] = 1
-    return x, y, z
+
+        idx = np.searchsorted(times, [t + yb1, t + yb2], side='right')
+        id_spiked_before = senders[idx[0]: idx[1]] - min_sender_ids
+
+        yb[i, id_spiked_before] = 1
+
+    return x, y, z, yb
 
 X = np.zeros((len(stim_times), N))
 Y = np.zeros((len(stim_times), N))
 Z = np.zeros((len(stim_times), N))
+Yb = np.zeros((len(stim_times), N))
 
 for f in path.iterdir():
     if not f.suffix == '.gdf':
@@ -52,11 +62,13 @@ for f in path.iterdir():
         continue
     df = pd.read_csv(f, sep='\t', header=None).rename(columns={0:'sender',1:'times'}).drop(columns=2)
 
-    x, y, z = make_regressors(stim_times, df)
+    x, y, z, yb = make_regressors(stim_times, df)
     X += x
     Y += y
     Z += z
+    Yb += yb
 
 np.save(path / 'X.npy', X)
 np.save(path / 'Y.npy', Y)
 np.save(path / 'Z.npy', Z)
+np.save(path / 'Yb.npy', Yb)
