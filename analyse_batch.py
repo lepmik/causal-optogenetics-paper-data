044519f9464e80519c6f6d7db132dd944ad1163e
Mikkel E Lepperød <lep.mik@gmail.com>
2020-03-01 09:04:31 +0100

    compute proper baseline

--- analyse_batch.py
+++ analyse_batch.py
@@ -13,19 +13,28 @@ warnings.filterwarnings("ignore")
 pandarallel.initialize(progress_bar=False)
 
 z1 = -2
-z2 = -1
+z2 = 0
 x1 = 1
 x2 = 3
 y1 = 3
-y2 = 7
-yb1 = -4
-yb2 = 0
+y2 = 10
+
+shift = y2
+
+zb1 = z1 - shift
+zb2 = z2 - shift
+xb1 = x1 - shift
+xb2 = x2 - shift
+yb1 = y1 - shift
+yb2 = y2 - shift
 
 # fnameout = path / 'conditional_means_3-7.feather'
 # fnameout = path / 'conditional_means_4-10.feather'
 # fnameout = 'conditional_means_z-(3_1)_y(3_7).feather'
 # fnameout = 'conditional_means_z-(2_1)_y(35_10).feather'
-fnameout = 'conditional_means_z-(2_1)_y(3_7).feather'
+# fnameout = 'conditional_means_z-(2_1)_y(3_7).feather'
+# fnameout = 'conditional_means_z-(2_1)_y(3_10).feather'
+fnameout = 'conditional_means_z-(2_0)_y(3_10).feather'
 
 def compute_response(row, stim_times, spikes, a, b):
     senders = spikes.senders
@@ -38,21 +47,23 @@ def compute_response(row, stim_times, spikes, a, b):
     return row
 
 
-def compute_conditional_means(row, X, Y, Z, Yb):
+def compute_conditional_means(row, X, Z, Y, Xb, Zb, Yb):
 
-    z = Z.loc[:, int(row.source)]
     x = X.loc[:, int(row.source)]
+    z = Z.loc[:, int(row.source)]
     y = Y.loc[:, int(row.target)]
+    xb = Xb.loc[:, int(row.source)]
+    zb = Zb.loc[:, int(row.source)]
     yb = Yb.loc[:, int(row.target)]
 
     y_ref = y[(z==1) & (x==0)].mean()
-    yb_ref = yb[(z==1) & (x==0)].mean()
+    yb_ref = yb[(zb==1) & (xb==0)].mean()
 
     y_base = y[x==0].mean()
-    yb_base = yb[x==0].mean()
+    yb_base = yb[xb==0].mean()
 
     y_respons = y[(z==0) & (x==1)].mean()
-    yb_respons = yb[(z==0) & (x==1)].mean()
+    yb_respons = yb[(zb==0) & (xb==1)].mean()
 
     return pd.Series({
         'n_ref': np.sum(z),
@@ -113,7 +124,6 @@ if __name__ == '__main__':
         if results is None:
             conn = pd.read_feather(path / 'connections_0.feather')
             sender_ids_ex = conn.query('weight >= 0').source.sort_values().unique() # excitatory neurons
-            assert min(sender_ids_ex) == 1
 
             results = conn.query(
                 'weight >= 0 and target in @sender_ids_ex and target not in @stim_nodes'
@@ -143,19 +153,24 @@ if __name__ == '__main__':
             np.zeros((len(stim_times), n_neurons)),
             columns=include_nodes
         )
-        Y, Z, Yb = X.copy(), X.copy(), X.copy()
+        Y, Z, Xb, Yb, Zb = X.copy(), X.copy(), X.copy(), X.copy(), X.copy()
         X = X.parallel_apply(
             compute_response, stim_times=stim_times, spikes=spikes, a=x1, b=x2, axis=1)
-        Y = Y.parallel_apply(
-            compute_response, stim_times=stim_times, spikes=spikes, a=y1, b=y2, axis=1)
         Z = Z.parallel_apply(
             compute_response, stim_times=stim_times, spikes=spikes, a=z1, b=z2, axis=1)
+        Y = Y.parallel_apply(
+            compute_response, stim_times=stim_times, spikes=spikes, a=y1, b=y2, axis=1)
+
+        Xb = Xb.parallel_apply(
+            compute_response, stim_times=stim_times, spikes=spikes, a=xb1, b=xb2, axis=1)
+        Zb = Zb.parallel_apply(
+            compute_response, stim_times=stim_times, spikes=spikes, a=zb1, b=zb2, axis=1)
         Yb = Yb.parallel_apply(
             compute_response, stim_times=stim_times, spikes=spikes, a=yb1, b=yb2, axis=1)
 
         results_ = results.join(results.parallel_apply(
             compute_conditional_means, axis=1,
-            X=X, Y=Y, Z=Z, Yb=Yb,
+            X=X, Z=Z, Y=Y, Xb=Xb, Zb=Zb, Yb=Yb,
             result_type='expand'
         ))
 
